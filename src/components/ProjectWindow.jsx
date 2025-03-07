51f48ff9bd411417f7e9227746293cf38ada6f6c
Evelyn Xiao <evelvnxiao@gmail.com>
2025-03-07 02:26:24 -0500

    fixed peojects page, currently updating info

--- src/components/ProjectWindow.jsx
+++ src/components/ProjectWindow.jsx
@@ -1,28 +1,18 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import ProjectsContainer from './ProjectsContainer.astro';
+import ProjectsContainer from '../components/ProjectsContainer.astro'; 
 
-
-const ProjectWindow = ( {currentPage} ) => {
+const ProjectWindow = ({ currentPage }) => {
     return (
         <>
-        {currentPage === '/projects' ? (
-            <>
-                <ProjectsContainer />
-            </>
-        ) : (
-          <>
-            
-          </> // Or something else you want to render
-        )}
-      </>
-        
+            {currentPage === '/projects' ? (
+                <ProjectsContainer/>
+            ) : (
+                <div>Other content or nothing to render</div> 
+            )}
+        </>
     );
-
-  
 };
-
-
 
 export default ProjectWindow;
